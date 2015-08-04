2a5233bc61444293c6f8bee63a5648f36bfe2bc8
timelyportfolio <kent.russell@timelyportfolio.com>
2015-08-04 14:28:46 -0500

    add min and max for tau_guide_x&y

--- R/taucharts.R
+++ R/taucharts.R
@@ -187,18 +187,22 @@ tau_guide_gridlines <- function(tau, show_x=TRUE, show_y=TRUE) {
 #'        information.
 #' @param tick_format can be any
 #'   \href{https://github.com/mbostock/d3/wiki/Formatting#d3_format}{D3 format specifier}
+#' @param min,max manual minimum and maximum for the axis
 #' @references \url{http://api.taucharts.com/basic/guide.html},
 #'             \url{https://github.com/mbostock/d3/wiki/Formatting#d3_format}
 #' @export
 tau_guide_x <- function(tau, padding=NULL,
                         label=NULL, label_padding=NULL,
-                        auto_scale=TRUE, tick_period=NULL, tick_format=NULL) {
+                        auto_scale=TRUE, tick_period=NULL, tick_format=NULL,
+                        min = NULL, max = NULL ) {
   tau$x$guide$x$autoScale <- auto_scale
   if (!is.null(label_padding)) tau$x$guide$x$label <- list(padding=label_padding)
   if (!is.null(label)) tau$x$guide$x$label$text <- label
   if (!is.null(tick_format)) tau$x$guide$x$tickFormat <- tick_format
   if (!is.null(tick_period)) tau$x$guide$y$tickPeriod <- tick_period
   if (!is.null(padding)) tau$x$guide$x$padding <- padding
+  if (!is.null(min)) tau$x$guide$x$min <- min
+  if (!is.null(max)) tau$x$guide$x$max <- max
   tau
 }
 
@@ -215,18 +219,22 @@ tau_guide_x <- function(tau, padding=NULL,
 #'        information.
 #' @param tick_format can be any
 #'   \href{https://github.com/mbostock/d3/wiki/Formatting#d3_format}{D3 format specifier}
+#' @param min,max manual minimum and maximum for the axis
 #' @references \url{http://api.taucharts.com/basic/guide.html},
 #'             \url{https://github.com/mbostock/d3/wiki/Formatting#d3_format}
 #' @export
 tau_guide_y <- function(tau, padding=NULL,
                         label=NULL, label_padding=NULL,
-                        auto_scale=TRUE, tick_period=NULL, tick_format=NULL) {
+                        auto_scale=TRUE, tick_period=NULL, tick_format=NULL,
+                        min=NULL,max=NULL) {
   tau$x$guide$y$autoScale <- auto_scale
   if (!is.null(label_padding)) tau$x$guide$y$label <- list(padding=label_padding)
   if (!is.null(label)) tau$x$guide$y$label$text <- label
   if (!is.null(tick_period)) tau$x$guide$y$tickPeriod <- tick_period
   if (!is.null(tick_format)) tau$x$guide$y$tickFormat <- tick_format
   if (!is.null(padding)) tau$x$guide$y$padding <- padding
+  if (!is.null(min)) tau$x$guide$y$min <- min
+  if (!is.null(max)) tau$x$guide$y$max <- max
   tau
 }
 
